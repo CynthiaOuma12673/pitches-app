6ea2c1374404a9c42438ae8e4cd5502d38da6d0c
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-03-05 17:05:00 +0300

    it will route to main and show index.html as the main template

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,17 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StrongField, PasswordField,SubmitField, ValidationError, BooleanField,TextAreaField
+from wtforms import StringField, PasswordField,SubmitField, ValidationError, BooleanField,TextAreaField
 from wtforms.validators import Required,Email,EqualTo
 
 class RegistrationForm(FlaskForm):
-    email = StrongField('Enter Your Email Address', validators=[Required(), Email()])
-    username = StrongField('Enter Your username', validators = [Required()])
+    email = StringField('Enter Your Email Address', validators=[Required(), Email()])
+    username = StringField('Enter Your username', validators = [Required()])
     password = PasswordField('Input password', validators = [Required(),EqualTo('password_confirm', message = 'Kindly input matching passwords')])
     password_confirm = PasswordField('Kindly confirm password', validators = [Required()])
     submit = SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
-    email = StrongField ('Enter Your Email Address', validators = [Required(), Email()])
+    email = StringField ('Enter Your Email Address', validators = [Required(), Email()])
     password = PasswordField('Input Password', validators = [Required()])
     remember = BooleanField('Save password')
     submit = SubmitField('Sign In')
-    
