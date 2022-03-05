aa7d31450069a2c0816c88a6d0fb83b356159b1f
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-03-05 16:09:38 +0300

    it will create the sign up form

---
+++ app/auth/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StrongField, PasswordField,SubmitField, ValidationError, BooleanField,TextAreaField
+from wtforms.validators import Required,Email,EqualTo
+
+class RegistrationForm(FlaskForm):
+    email = StrongField('Enter Your Email Address', validators=[Required(), Email()])
+    username = StrongField('Enter Your username', validators = [Required()])
+    password = PasswordField('Input password', validators = [Required(),EqualTo('password_confirm', message = 'Kindly input matching passwords')])
+    password_confirm = PasswordField('Kindly confirm password', validators = [Required()])
+    submit = SubmitField('Sign Up')
