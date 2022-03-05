1c3ef8f573834dd5df81b94a58eb8c9ab1782389
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-03-05 11:50:24 +0300

    it will create a function that will create app configs and initialize the flask extensions

--- app/__init__.py
+++ app/__init__.py
@@ -10,10 +10,20 @@ from flask_simplemde import SimpleMDE
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
-
 simple = SimpleMDE()
-
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 photos = UploadSet('photos',IMAGES)
 mail = Mail()
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+
