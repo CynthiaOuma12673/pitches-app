724450ff3a5ebe95a41caf0b5e3db6618e2dd908
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-03-05 16:13:39 +0300

    it will create the sign in form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -8,3 +8,10 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Input password', validators = [Required(),EqualTo('password_confirm', message = 'Kindly input matching passwords')])
     password_confirm = PasswordField('Kindly confirm password', validators = [Required()])
     submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    email = StrongField ('Enter Your Email Address', validators = [Required(), Email()])
+    password = PasswordField('Input Password', validators = [Required()])
+    remember = BooleanField('Save password')
+    submit = SubmitField('Sign In')
+    
