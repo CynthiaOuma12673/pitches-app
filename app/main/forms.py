2f0501280cf2f103e8300d19dbedd41ca82278c7
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-03-05 16:53:50 +0300

    it will create a class that will allow the user to leave a comment in the app

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,3 +6,6 @@ class UpdateProfile(FlaskForm):
     bio = TextAreaField('Kindly let us know you better.', validators = [Required()])
     submit = SubmitField('Save')
 
+class CommentForm(FlaskForm):
+    comment = TextAreaField ('Kindly leave a comment', validators = [Required()])
+    submit = SubmitField('Comment')
