e0c05c0fb9257f24e519985817e3a6c52a190238
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-03-06 09:48:36 +0300

    it will add the routes to the comments in the views file

--- app/main/views.py
+++ app/main/views.py
@@ -15,3 +15,17 @@ def index():
     technology = Pitch.query.filter_by(category = 'Technology')
     
     return render_template('index.html', interview = interview, product = product, pitches = pitches,technology = technology, advertisement = advertisement)
+
+@main.route('/comment/<int:pitch_id>',methods = ['POST', 'GET'])
+@login_required
+def comment(pitch_id):
+    form = CommentForm()
+    pitch = Pitch.query.get(pitch_id)
+    comments = Comment.query.filter_by(pitch_id = pitch_id).all()
+    if form.validate_on_submit():
+        comment = form.comment.data
+        pitch_id = pitch_id
+        new_comment = Comment(comment = comment, pitch_id = pitch_id, user = current_user)
+        new_comment.save_comment()
+        return redirect(url_for('.comment', pitch_id = pitch_id))
+    return render_template('comment.html', form = form, pitch = pitch, comments = comments)
