bd7619048b915d6b1949ed79d1ee4ed83cdd6d68
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-03-06 09:33:36 +0300

    it will add more imports to the views file

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,10 @@
 from flask import render_template, request, redirect, url_for,abort
 from . import main
+from flask_login import login_required,current_user
+from ..models import User, Pitch, Comment, Upvote, Downvote
+from .forms import UpdateProfile, PitchForm, CommentForm
+from ..import db,photos
+import datetime
 
 @main.route('/')
 def index():
